00794183f6e4fa9a808f37a1f139fbab718c40f1
TheSuperiorStanislav <stasontos2@hotmail.com>
2019-02-23 15:00:50 +0700

    add CRUD

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
-from .views import ArticleListView
+from .views import (
+    ArticleListView,
+    ArticleDetailView,
+    ArticleCreateView,
+    ArticleUpdateView,
+    ArticleDeleteView,
+)
 
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='article_list')
+    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('<int:pk>/update/', ArticleUpdateView.as_view(), name='article_edit'),
+    path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('new/', ArticleCreateView.as_view(), name='article_new'),
+    path('', ArticleListView.as_view(), name='article_list'),
 ]
